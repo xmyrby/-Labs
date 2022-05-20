8fbd0e6d995ccdc0cdb0132343503e61157ef6db
xmyrby <djzombie524@gmail.com>
2022-05-21 01:27:45 +0300

    6.1 Fix

--- Python/6-1-Zhukov.py
+++ Python/6-1-Zhukov.py
@@ -2,15 +2,7 @@ N = int(input("Введите число: "))
 
 while(N<=0):
     N = int(input("Введите число: "))
-K=0
-
-i = 1
-while(N>0):
-    if(N%10%2):
-        K+=N%10*i
-        i*=10
-    N//=10
-if(i == 1):
-    print("Цифр кратных 2 нет")
-else:
-    print(K)
+s=str(N)
+for i in range(2,10,2):
+    s = s.replace(str(i),'')
+print('Число :',s)
