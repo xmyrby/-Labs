16be771c8b1870c5f6a672998e92eaa4c80101b8
xmyrby <djzombie524@gmail.com>
2022-05-28 02:14:05 +0300

    Enemies

--- SDL/RogueLike/RogueLike/Main.cpp
+++ SDL/RogueLike/RogueLike/Main.cpp
@@ -14,19 +14,34 @@ int winHgt = 960;
 
 int moves = 0;
 
-struct Player
+const int MAP_SIZE = 300;
+const int CELLS_DENSITY = 12;
+const int CELLS_FILL = 6000;
+const int ANTS_COUNT = 6;
+const int ENEMIES_COUNT = 20;
+
+struct Entity
 {
 	int x, y;
+	int health;
+};
+
+struct Player : Entity
+{
 	int moves = 1;
 	int attacks = 1;
 };
 
-Player player = { 150,150 };
+struct Enemy : Entity
+{
+	int type = 0;
+};
 
-const int mapSize = 300;
-const int cellsDensity = 12;
-const int cellsFill = 6000;
-const int antsCount = 6;
+Player player;
+
+Enemy enemies[ENEMIES_COUNT];
+
+int map[MAP_SIZE][MAP_SIZE];
 
 void DeInit(char error)
 {
@@ -83,7 +98,7 @@ void Init()
 	LoadTextures();
 }
 
-bool CheckCell(int map[mapSize][mapSize], int x, int y)
+bool CheckCell(int x, int y)
 {
 	int weight = 0;
 	for (int i = x; i < x + 5; i++)
@@ -91,34 +106,34 @@ bool CheckCell(int map[mapSize][mapSize], int x, int y)
 			if (map[i][j] != 0)
 				weight++;
 
-	if (weight < cellsDensity)
+	if (weight < CELLS_DENSITY)
 		return false;
 	return true;
 }
 
-void Generate(int map[mapSize][mapSize])
+void Generate()
 {
 	struct Ant
 	{
 		int x, y;
 	};
 
-	for (int i = 0; i < mapSize; i++)
-		for (int j = 0; j < mapSize; j++)
+	for (int i = 0; i < MAP_SIZE; i++)
+		for (int j = 0; j < MAP_SIZE; j++)
 			map[i][j] = 1;
 
-	Ant ants[antsCount];
+	Ant ants[ANTS_COUNT];
 	int cells = 0;
 
-	for (int i = 0; i < antsCount; i++)
+	for (int i = 0; i < ANTS_COUNT; i++)
 	{
-		ants[i] = { rand() % (mapSize / 2) + (mapSize / 4), rand() % (mapSize / 2) + (mapSize / 4) };
+		ants[i] = { rand() % (MAP_SIZE / 2) + (MAP_SIZE / 4), rand() % (MAP_SIZE / 2) + (MAP_SIZE / 4) };
 		map[ants[i].x][ants[i].y] = 0;
 		cells++;
 	}
 
-	while (cells < cellsFill)
-		for (int j = 0; j < antsCount; j++)
+	while (cells < CELLS_DENSITY)
+		for (int j = 0; j < ANTS_COUNT; j++)
 		{
 			Ant ant = ants[j];
 
@@ -133,7 +148,7 @@ void Generate(int map[mapSize][mapSize])
 						ant.x--;
 					break;
 				case 1:
-					if (ant.x < mapSize - 2)
+					if (ant.x < MAP_SIZE - 2)
 						ant.x++;
 					break;
 				case 2:
@@ -141,13 +156,13 @@ void Generate(int map[mapSize][mapSize])
 						ant.y--;
 					break;
 				case 3:
-					if (ant.y < mapSize - 2)
+					if (ant.y < MAP_SIZE - 2)
 						ant.y++;
 					break;
 				default:
 					break;
 				}
-				if (map[ant.x][ant.y] != 0 && CheckCell(map, ant.x - 2, ant.y - 2))
+				if (map[ant.x][ant.y] != 0 && CheckCell(ant.x - 2, ant.y - 2))
 				{
 					map[ant.x][ant.y] = 0;
 					cells++;
@@ -156,7 +171,7 @@ void Generate(int map[mapSize][mapSize])
 		}
 }
 
-int GetTile(int map[mapSize][mapSize], int i, int j)
+/*int GetTile(int i, int j)
 {
 	if (map[i - 1][j] == 0 && map[i + 1][j] != 0 && map[i][j - 1] == 0 && map[i][j + 1] != 0 && map[i + 1][j + 1] != 0)
 		return 0;
@@ -191,14 +206,16 @@ int GetTile(int map[mapSize][mapSize], int i, int j)
 	if (map[i - 1][j] == 0 && map[i + 1][j] != 0 && map[i][j + 1] == 0 && map[i][j - 1] != 0 && map[i + 1][j - 1] == 0)
 		return 16;
 	return 8;
-}
+}*/
 
-int RayTracing(int map[mapSize][mapSize], int x, int y)
+int RayTracing(int x, int y)
 {
 	float ax = x, ay = y;
 	float kx = player.x - x, ky = player.y - y;
 	int lastx = x, lasty = y;
 	int blocks = 0;
+	if (abs(kx) == abs(ky))
+		blocks++;
 	while (x != player.x || y != player.y)
 	{
 		ax += kx / 40;
@@ -225,7 +242,7 @@ int Max(int a, int b)
 	return b;
 }
 
-void Draw(int map[mapSize][mapSize])
+void Draw()
 {
 	SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
 	SDL_RenderClear(ren);
@@ -234,7 +251,7 @@ void Draw(int map[mapSize][mapSize])
 		for (int j = player.y - 15; j < player.y + 16; j++)
 		{
 			SDL_Rect rect = { (i + 15 - player.x) * 32 - 16,(j + 15 - player.y) * 32 - 16,32,32 };
-			int blocks = RayTracing(map, i, j);
+			int blocks = RayTracing(i, j);
 			if (map[i][j] == 1)
 			{
 				SDL_SetRenderDrawColor(ren, Max(15, 138 - blocks * 16), Max(15, 22 - blocks), Max(15, 31 - blocks * 2), 255);
@@ -250,15 +267,39 @@ void Draw(int map[mapSize][mapSize])
 	SDL_Rect rect = { 464, 464,32,32 };
 	SDL_SetRenderDrawColor(ren, 114, 230, 221, 255);
 	SDL_RenderFillRect(ren, &rect);
+
+	SDL_SetRenderDrawColor(ren, 114, 230, 221, 255);
+	for (int i = 0; i < ENEMIES_COUNT; i++)
+	{
+		int blocks = RayTracing(enemies[i].x, enemies[i].y);
+		rect = { (enemies[i].x + 15 - player.x) * 32 - 16, (enemies[i].y + 15 - player.y) * 32 - 16,32,32 };
+		SDL_SetRenderDrawColor(ren, Max(15, 221 - blocks * 27), Max(15, 230 - blocks * 28), Max(15, 114 - blocks * 13), 255);
+		SDL_RenderFillRect(ren, &rect);
+	}
 }
 
-void SpawnPlayer(int map[mapSize][mapSize])
+void SpawnPlayer()
 {
 	do
 	{
-		player.x = rand() % mapSize;
-		player.y = rand() % mapSize;
+		player.x = rand() % MAP_SIZE;
+		player.y = rand() % MAP_SIZE;
 	} while (map[player.x][player.y] != 0);
+	player.health = 10;
+}
+
+void SpawnEnemies()
+{
+	for (int i = 0; i < ENEMIES_COUNT; i++)
+	{
+		Enemy enemy;
+		do
+		{
+			enemy.x = rand() % MAP_SIZE;
+			enemy.y = rand() % MAP_SIZE;
+		} while (map[enemy.x][enemy.y] != 0);
+		enemies[i] = enemy;
+	}
 }
 
 int GetSize(const char* text)
@@ -267,14 +308,6 @@ int GetSize(const char* text)
 	while (text[count] != '\0')
 		count++;
 	return count;
-}
-
-const char* CC(int var)
-{
-	char buff[10];
-
-	_itoa_s(var, (char*)buff, 10, 10);
-	return buff;
 }
 
 void PrintText(const char* text, int posx, int posy, int size)
@@ -316,11 +349,36 @@ void RenderImage(int textureId, int x, int y, int w, int h)
 void DrawUI()
 {
 	PrintText("Move: #", 10, 18, 16);
-	PrintText(moves+1, 96, 18, 16);
+	PrintText(moves + 1, 96, 18, 16);
 	RenderImage(0, 10, 52, 32, 32);
 	PrintText(player.attacks, 52, 60, 16);
 	RenderImage(1, 10, 94, 32, 32);
 	PrintText(player.moves, 52, 104, 16);
+	PrintText("Health: ", 10, 926, 16);
+	PrintText(player.health, 102, 926, 16);
+}
+
+void MakeEnemyMove(Enemy& enemy)
+{
+	if (RayTracing(player.x, player.y) <= 5)
+	{
+		if (enemy.x < player.x && map[enemy.x + 1][enemy.y] == 0)
+		{
+			enemy.x++;
+		}
+		else if (enemy.x > player.x && map[enemy.x - 1][enemy.y] == 0)
+		{
+			enemy.x--;
+		}
+		else if (enemy.y < player.y && map[enemy.x][enemy.y + 1] == 0)
+		{
+			enemy.y++;
+		}
+		else if (enemy.y > player.y && map[enemy.x][enemy.y - 1] == 0)
+		{
+			enemy.y--;
+		}
+	}
 }
 
 void CheckMove()
@@ -330,6 +388,11 @@ void CheckMove()
 		moves++;
 		player.moves++;
 		player.attacks++;
+
+		for (int i = 0; i < ENEMIES_COUNT; i++)
+		{
+			MakeEnemyMove(enemies[i]);
+		}
 	}
 }
 
@@ -337,15 +400,15 @@ void CheckMove()
 int main()
 {
 	srand(time(NULL));
-	int map[mapSize][mapSize];
 	Init();
 	font = TTF_OpenFont("Fonts\\MainFont.ttf", 20);
 	SDL_Event event;
 
-	Generate(map);
-	SpawnPlayer(map);
+	Generate();
+	SpawnPlayer();
+	SpawnEnemies();
 
-	Draw(map);
+	Draw();
 
 	while (true)
 	{
@@ -354,7 +417,7 @@ int main()
 
 		if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP)
 		{
-			if (state[SDL_SCANCODE_UP])
+			if (state[SDL_SCANCODE_W])
 			{
 				if (map[player.x][player.y - 1] == 0 && player.moves > 0)
 				{
@@ -362,7 +425,7 @@ int main()
 					player.moves--;
 				}
 			}
-			else if (state[SDL_SCANCODE_DOWN])
+			else if (state[SDL_SCANCODE_S])
 			{
 				if (map[player.x][player.y + 1] == 0 && player.moves > 0)
 				{
@@ -370,7 +433,7 @@ int main()
 					player.moves--;
 				}
 			}
-			else if (state[SDL_SCANCODE_LEFT])
+			else if (state[SDL_SCANCODE_A])
 			{
 				if (map[player.x - 1][player.y] == 0 && player.moves > 0)
 				{
@@ -378,7 +441,7 @@ int main()
 					player.moves--;
 				}
 			}
-			else if (state[SDL_SCANCODE_RIGHT])
+			else if (state[SDL_SCANCODE_D])
 			{
 				if (map[player.x + 1][player.y] == 0 && player.moves > 0)
 				{
@@ -394,7 +457,7 @@ int main()
 
 			CheckMove();
 
-			Draw(map);
+			Draw();
 		}
 		DrawUI();
 
