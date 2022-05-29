2e1a444ffead7f34c9363992ff79bb289fc404b9
xmyrby <djzombie524@gmail.com>
2022-05-30 01:21:42 +0300

    8-2

---
+++ Python/8-2-Zhukov.py
@@ -0,0 +1,11 @@
+f = open('8-2-7.txt', 'r')
+s = f.read()
+sub = 'BOSS'
+pos = s.find(sub)
+k = 0
+while pos != -1:
+    if s[pos-1] != 'J' or s[pos+4] != 'J':
+        if s[pos-1] != 'J' and s[pos+4] != 'J':
+            k += 1
+    pos = s.find(sub, pos + 1)
+print(k)
