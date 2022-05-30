1b5e549f31e0c376d03e5f2b67e0c09f3f2faa13
xmyrby <djzombie524@gmail.com>
2022-05-31 00:02:56 +0300

    Update Maks4.py

--- Python/Maks4.py
+++ Python/Maks4.py
@@ -1,11 +1,9 @@
 import math
 print("_________________________________\n")
-x=0.1
-while(x + 0.03 <= 0.85):
+for x in (z / 100 for z in range(10,86,3)):
     if(math.cos(30*math.pi/180)**2 - math.e**(math.sin(x)**0.5) > -1.2):
-        print(f"| X = {x:.3f} | Y = {math.cos(30*math.pi/180) + math.sin(x)**0.5:.3f} | > -1.2 |")
+        print(f"| X = {x:.2f} | Y = {math.cos(30*math.pi/180) + math.sin(x)**0.5:.3f} | > -1.2 |")
     else:
-        print(f"| X = {x:.3f} | Y = {math.cos(30*math.pi/180)**0.5/math.log(math.sin(x)**0.5):.3f} | <= -1.2 |")
-    x += 0.03
+        print(f"| X = {x:.2f} | Y = {math.cos(30*math.pi/180)**0.5/math.log(math.sin(x)**0.5):.3f} | <= -1.2 |")
     
 print("_________________________________\n")
