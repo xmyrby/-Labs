9062a9ea2c77321024e4763a1de61c6ae3c4bac5
xmyrby <djzombie524@gmail.com>
2022-05-30 23:59:05 +0300

    Update Maks2.py

--- Python/Maks2.py
+++ Python/Maks2.py
@@ -1,9 +1,9 @@
 A = int(input("Введите A (A > 3): "))
-B = 0
 s = 0
-while(s+B+1 < A):
-    B+=1
+for B in range(A//2):
     s+=B
+    if(s+B+1 >= A):
+        break
 
-print("B = ",B)
+print("B =",B)
     
