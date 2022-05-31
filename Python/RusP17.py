0f446e71a13eec0b771ff22b8ca3ec49d53616c4
xmyrby <djzombie524@gmail.com>
2022-05-31 22:43:13 +0300

    Update RusP17.py

--- Python/RusP17.py
+++ Python/RusP17.py
@@ -8,17 +8,20 @@ def getSize():
 
 s = getSize()
 x1 = {random.randrange(1,1001) for i in range(s)}
+print(x1)
 s = getSize()
 x2 = {random.randrange(1,1001) for i in range(s)}
-s = getSize()
-x3 = {random.randrange(1,1001) for i in range(s)}
-y = (x2.union(x3)).difference(x1.union(x2).intersection(x1.union(x3)))
-print('x1 =',x1)
-print('x2 =',x2)
-print('x3 =',x3)
-print('y =',y)
+print(x2)
 
-y1 = {i for i in range(13,1001)}
-y1 = y1.intersection(y)
-print('y1 =',y1)
-print('Мощность y1 =',len(y1э))
+y1 = {i*3 for i in range(1,334)}
+y1=y1.intersection(x1)
+
+y2 = {i*5 for i in range(1,201)}
+y2 = y2.intersection(x2)
+
+y3 = y2.intersection(y1)
+
+if(y3 == set()):
+    print('Пустое множество')
+else:
+    print('Полученное множество:\n',y3,'\nМощность',len(y3))
