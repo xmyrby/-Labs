cf434f6a7081c65c36e1fd925ed59c6a62c4158f
xmyrby <djzombie524@gmail.com>
2022-06-09 02:15:59 +0300

    Fix

--- Sharp/VK BOT Sectum/VK BOT Sectum/Program.cs
+++ Sharp/VK BOT Sectum/VK BOT Sectum/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using VkNet;
 using VkNet.Enums.SafetyEnums;
 using VkNet.Model;
 using VkNet.Model.RequestParams;
 using System.IO;
 using System.Diagnostics;
+using VkNet.Enums.Filters;
+using System.Linq;
+using System.Threading;
 
 namespace VK_BOT_Sectum
 {
@@ -30,15 +32,18 @@ namespace VK_BOT_Sectum
             }
 
         }
-
-        static bool CheckId(VkNet.Utils.VkCollection<User> us, long? id)
+        static string GetVkName(long? id)
         {
-            for (int i = 0; i < us.Count; i++)
+            try
             {
-                if (id == us[i].Id)
-                    return true;
+                var user = api.Users.Get(new long[] { (long)(id) }, ProfileFields.FirstName | ProfileFields.LastName);
+
+                return user[0].FirstName + " " + user[0].LastName;
             }
-            return false;
+            catch
+            {
+                return "[GetVkName ERROR, Input id probably 0]";
+            }
         }
         static void Main()
         {
@@ -50,27 +55,37 @@ namespace VK_BOT_Sectum
             {
                 while (true)
                 {
-                    var us = api.Groups.GetMembers(new GroupsGetMembersParams() { GroupId = "210381180" });
                     var conv = api.Messages.GetConversations(new GetConversationsParams() { Filter = GetConversationFilter.Unread }).Items;
 
                     int count = conv.Count;
                     for (int i = 0; i < count; i++)
                     {
                         long? id = conv[i].LastMessage.FromId;
-                        if (CheckId(us, id))
+                        if (api.Groups.IsMember("210381180", id, null, null).Select(x => x.Member).FirstOrDefault())
                         {
                             try
                             {
                                 StreamReader reader = new StreamReader(conv[i].LastMessage.Text + ".txt");
                                 string Answer = reader.ReadToEnd();
-                                do
+                                if (Answer.Length > 4096)
                                 {
-                                    SendMessage(id, Answer.Substring(0, Math.Min(Answer.Length, 4096)));
-                                    if (Answer.Length > 4096)
-                                        Answer = Answer.Substring(4096, Answer.Length - 4096);
+                                    int c = Answer.Length / 4096 + 1;
+                                    for (int j = 0; j < c; j++)
+                                    {
+                                        SendMessage(id, Answer.Substring(0, Math.Min(Answer.Length, 4096)));
+                                        if (Answer.Length > 4096)
+                                            Answer = Answer.Substring(4096, Answer.Length - 4096);
+                                    }
                                 }
-                                while (Answer.Length > 4096);
+                                else
+                                {
+                                    SendMessage(id, Answer);
+                                }
                                 reader.Close();
+
+                                StreamWriter writer = new StreamWriter("Logs.txt", true);
+                                writer.WriteLine(DateTime.Now + " " + GetVkName(id) + " asked question #" + conv[i].LastMessage.Text);
+                                writer.Close();
                             }
                             catch (Exception e)
                             {
@@ -82,11 +97,15 @@ namespace VK_BOT_Sectum
                             SendMessage(id, "Подпишись");
                         }
                     }
+                    Thread.Sleep(1000);
                 }
             }
             catch (Exception e)
             {
-                Process.Start("VK BOT INSPECTOR.exe", rnd.Next(0, 100).ToString());
+                StreamWriter writer = new StreamWriter("Logs.txt", true);
+                writer.WriteLine(e.ToString());
+                writer.Close();
+                Process.Start("VK BOT Sectum.exe", rnd.Next(0, 100).ToString());
             }
         }
     }
