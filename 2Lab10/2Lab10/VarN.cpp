ca5684e615bdc71b22fa167023c25a6c6833ba87
xmyrby <djzombie524@gmail.com>
2022-06-14 03:56:14 +0300

    10_7

---
+++ 2Lab10/2Lab10/VarN.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+char* RemoveNumbers(char* str)
+{
+	char newStr[80];
+	int ln = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		int symb = int(str[i]);
+		if (symb < 48 || symb > 57)
+		{
+			newStr[ln] = str[i];
+			ln++;
+		}
+	}
+	newStr[ln] = '\0';
+	return newStr;
+}
+
+int main()
+{
+	char str[80];
+	gets_s(str);
+	printf("%s", RemoveNumbers(str));
+
+	return 0;
+}
