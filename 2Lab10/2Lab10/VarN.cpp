18bd73c8943450cd83016f765f5a69cb748131b3
xmyrby <djzombie524@gmail.com>
2022-06-14 04:05:24 +0300

    т16

--- 2Lab10/2Lab10/VarN.cpp
+++ 2Lab10/2Lab10/VarN.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <iostream>
-
+//7
 char* RemoveNumbers(char* str)
 {
 	char newStr[80];
---
+++ 2Lab10/N25/Var10N.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+//16
+char* RemoveSpaces(char* str)
+{
+	char newStr[80];
+	int ln = 0;
+	int count = 1;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+			count++;
+		else
+			count = 0;
+
+		if (count < 2)
+		{
+			newStr[ln] = str[i];
+			ln++;
+		}
+	}
+	newStr[ln] = '\0';
+	return newStr;
+}
+
+int main()
+{
+	char str[80];
+	gets_s(str);
+	printf("%s", RemoveSpaces(str));
+
+	return 0;
+}
