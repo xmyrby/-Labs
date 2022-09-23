9f576dff71d78e2d6e491614ecf628c4ec12ee42
xmyrby <djzombie524@gmail.com>
2022-09-23 16:09:14 +0300

    Maks2-2

--- Maks2-2/Maks2-2/Source.cpp
+++ Maks2-2/Maks2-2/Source.cpp
@@ -2,12 +2,14 @@
 
 int main()
 {
-	int* number1 = 0;
-	int* number2 = 0;
+	int number1 = 0;
+	int number2 = 0;
+	int* n1 = &number1;
+	int* n2 = &number2;
 
-	scanf_s("%d %d", number1, number2);
-	int sum = *number1 + *number2;
-	int prod = *number1 * *number2;
+	scanf_s("%d %d", &number1, &number2);
+	int sum = *n1 + *n2;
+	int prod = *n1 * *n2;
 	printf("%d\n%d", sum, prod);
 
 	return 0;
