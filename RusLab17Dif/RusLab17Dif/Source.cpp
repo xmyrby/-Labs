a0a6da61333d17344a3679b9bea27a6baab540d3
xmyrby <djzombie524@gmail.com>
2022-11-06 19:38:17 +0300

    RusComments

--- RusLab17Dif/RusLab17Dif/Source.cpp
+++ RusLab17Dif/RusLab17Dif/Source.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 
+//Свап указателей на значения чисел
 void swap(int* a, int* b)
 {
 	int temp = *a;
@@ -7,6 +8,7 @@ void swap(int* a, int* b)
 	*b = temp;
 }
 
+//Вывод массива в консоль
 void PrintMas(int* mas, int size)
 {
 	for (int i = 0; i < size; i++)
@@ -16,11 +18,13 @@ void PrintMas(int* mas, int size)
 	printf("\n");
 }
 
+//Способ 1
 void WayDif_0(int* mas0, int* mas1, int* mas2, int sizes[3])
 {
 	int size = sizes[0] + sizes[1] + sizes[2];
 	int* mas = (int*)malloc(sizeof(int) * size);
 
+	//Перенос в 1 массив значений всех массивов
 	for (int i = 0; i < size; i++)
 	{
 		if (i < sizes[0])
@@ -31,11 +35,13 @@ void WayDif_0(int* mas0, int* mas1, int* mas2, int sizes[3])
 			mas[i + sizes[0] + sizes[1]] = mas2[i];
 	}
 
+	//Сортировка пузырьком
 	for (int i = 0; i < size - 1; i++)
 		for (int j = 0; j < size - i - 1; j++)
 			if (mas[j] > mas[j + 1])
 				swap(&mas[j], &mas[j + 1]);
 
+	//Возвращение в массивы отсортированных значений
 	for (int i = 0; i < size; i++)
 	{
 		if (i < sizes[0])
@@ -48,6 +54,7 @@ void WayDif_0(int* mas0, int* mas1, int* mas2, int sizes[3])
 	printf("\n");
 }
 
+//Задание размера
 void SetSize(int& size)
 {
 	do
@@ -58,6 +65,7 @@ void SetSize(int& size)
 	printf("\n");
 }
 
+//Заполнение массива
 void FillMas(int* mas, int size)
 {
 	for (int i = 0; i < size; i++)
@@ -74,6 +82,7 @@ int main()
 
 	int sizes[3];
 
+	//Создание массивов и выделение размера в памяти
 	SetSize(sizes[0]);
 	int* mas0 = (int*)malloc(sizeof(int) * sizes[0]);
 	FillMas(mas0, sizes[0]);
@@ -86,10 +95,12 @@ int main()
 	int* mas2 = (int*)malloc(sizeof(int) * sizes[2]);
 	FillMas(mas2, sizes[2]);
 
+	//Вывод массивов
 	PrintMas(mas0, sizes[0]);
 	PrintMas(mas1, sizes[1]);
 	PrintMas(mas2, sizes[2]);
 
+	//Работа над массивами и повторный вывод
 	WayDif_0(mas0, mas1, mas2, sizes);
 
 	PrintMas(mas0, sizes[0]);
